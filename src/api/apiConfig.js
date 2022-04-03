030fcbfab172a9b5c33484b31a6a31adc556af06
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-04-03 16:58:47 +0900

    fix/typo

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,7 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
   apiKey: "37ed43a4f8eaa2abd75f9283692947bc",
-  originalImage: (imgPath) => `https://image.tmdb.org/t/p/originl/${imgPath}`,
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
   w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 };
 
